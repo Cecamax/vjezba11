105a3c1b33d9f41735772542e2ad8ccd64b24513
Cecamax <57287400+Cecamax@users.noreply.github.com>
2020-01-25 16:34:38 +0100

    Add files via upload

---
+++ vjezba11.php
@@ -0,0 +1,9 @@
+<?php
+
+for($i = 0; $i < 10; $i++){
+    echo "<div style='background-color:red'>";
+        echo "<p style='color:white'>";
+            echo "naslov broj $i";
+        echo "</p>";
+    echo "</div>";
+}
